5cf85785ae26f37ce34a49a2787736f6d749b555
Outros Usuários <OutrosUsuarios@Andrezas-iMac.local>
2020-03-31 13:01:58 -0300

    aggregate

--- src/app/Controllers/OpportunityController.js
+++ src/app/Controllers/OpportunityController.js
@@ -1,12 +1,30 @@
 const Model = require("../Models/Opportunities");
 
 exports.get = function(req, res, next) {
-  Model.find({}, function(err, data) {
-    if (err) {
-      return res.status(400).json({
-        error: err
-      });
+  Model.aggregate(
+    [
+      {
+        $project: {
+          total: "$total",
+          created_at: {
+            $dateToString: { format: "%Y-%m-%d", date: "$created_at" }
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { created_at: "$created_at", total: "$total" },
+          count: { $sum: 1 }
+        }
+      }
+    ],
+    function(err, data) {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+      return res.status(200).json(data);
     }
-    return res.status(200).json(data);
-  });
+  );
 };
